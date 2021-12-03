781776b9d3c069f6fb563a8f41e2e2272f73da06
Evryon <79467907+Evryon75@users.noreply.github.com>
2021-12-03 16:39:05 +0100

    COLLISIONS ACTUALLY WORKS WTFWTWFWT

--- Javascript/Main.js
+++ Javascript/Main.js
@@ -30,42 +30,18 @@ function updateGameArea() {
     myGameArea.draw(wallLeft);
     myGameArea.draw(wallRight);
     myGameArea.draw(block);
-    myGameArea.draw(box);
+    myGameArea.draw(box1);
+    myGameArea.draw(box2);
+    myGameArea.draw(box3);
 
     gravity();
 
-    if (player.x + player.width > wallRight.x - 5){
-        player.x -= speedRight;
-        onWall = true;
-    } else {
-        onWall = false;
-    }
+    wallRightCollision();
+    wallLeftCollision();
 
-    if (player.x < wallLeft.x + 30){
-        player.x += speedLeft;
-        onWall = true;
-    } else {
-        onWall = false;
-    }
-
-    if (player.x + player.width + 10 > box.x & player.x < box.x + box.width & player.y + player.height > box.y) {
-        
-        player.x -= speedRight;
-    }
-
-    if (player.x < box.x + box.width  + 10 & player.x > box.x & player.y + player.height > box.y) {
-        
-        player.x += speedLeft;
-    }
-
-    if (player.x > box.x & player.x < box.x + box.width & player.y + player.height > box.y) {
-        
-        player.y = box.y - box.height;
-        ground = true;
-        airTime = 0;
-        moveright();
-        moveleft();
-    }
+    boxCollision(box1);
+    boxCollision(box2);
+    boxCollision(box3);
 
     moveright();
     moveleft();
@@ -86,4 +62,178 @@ function gravity(){
         fallSpeed = 10;
         airTime = 0;
     }
+}
+function wallRightCollision(){
+    if (player.x + player.width > wallRight.x - 5){
+        player.x -= speedRight;
+        onWall = true;
+    } else {
+        onWall = false;
+    }
+}
+function wallLeftCollision(){
+    if (player.x < wallLeft.x + 30){
+        player.x += speedLeft;
+        onWall = true;
+    } else {
+        onWall = false;
+    }
+}
+function boxCollision(boxThing){
+    if ((player.x + player.width) > boxThing.x & player.x < (boxThing.x + boxThing.width)){
+        
+        if (player.y + player.height > boxThing.y){
+            player.y -= 5;
+            ground = true;
+            airTime = 0;
+        }
+    }
+
+    let playerWidth = player.x + player.width +5;
+    let boxWidth = boxThing.x + boxThing.width;
+    let playerHeight = player.y + player.height
+
+    if (playerWidth > boxThing.x & playerHeight > boxThing.y & player.x < boxWidth){
+        player.x -= speedRight;
+    }
+    if (player.x - 10 < boxWidth & playerHeight > boxThing.y & player.x > boxThing.x){
+        player.x += speedLeft;
+    }
+}
+
+//GAMEOBJECTS//
+var player = {
+    width: 40,
+    height: 80,
+    x: 500,
+    y: 520,
+    color: "red",
+    facing: "RIGHT"
+};
+var block = {
+    width: 1450,
+    height: 20,
+    x: 0,
+    y: 600,
+    color: "black"
+};
+var wallLeft = {
+    width: 20,
+    height: 300,
+    x: 200,
+    y: 300,
+    color: "blue"
+};
+var wallRight = {
+    width: 20,
+    height: 300,
+    x: 1200,
+    y: 300,
+    color: "blue"
+};
+var box1 = {
+    width: 80,
+    height: 80,
+    x: 800,
+    y: 520,
+    color: "pink"
+};
+var box2 = {
+    width: 180,
+    height: 120,
+    x: 880,
+    y: 480,
+    color: "pink"
+};
+var box3 = {
+    width: 140,
+    height: 40,
+    x: 1060,
+    y: 560,
+    color: "pink"
+};
+
+//MOVEMENT//
+let fallSpeed = 10;
+let onWall = false;
+let ground = true;
+let airTime = 0;
+
+let speedRight = 0;
+let speedLeft = 0;
+let speedUp = 0;
+let speedDown = 0;
+
+document.addEventListener('keydown', (event) => {
+    
+    //ArrowDown ArrowRight ArrowUp ArrowLeft
+
+    switch(event.key) {
+
+        case "ArrowDown":
+            speedDown = 0;
+            
+        break;
+        
+        case "ArrowRight":
+            speedRight = 10;
+            player.facing = "RIGHT"
+            
+        break;
+
+        case "a":
+            speedUp = 15;
+            
+        break;
+
+        case "ArrowLeft":
+            speedLeft = 10;
+            player.facing = "LEFT"
+            
+        break;
+    }
+});
+document.addEventListener('keyup', (event) => {
+    
+    //ArrowDown ArrowRight ArrowUp ArrowLeft
+
+    switch(event.key) {
+
+        case "ArrowDown":
+            speedDown = 0;
+            
+        break;
+        
+        case "ArrowRight":
+            speedRight = 0;
+            
+        break;
+
+        case "a":
+            speedUp = 0;
+            airTime = 162;
+        break;
+
+        case "ArrowLeft":
+            speedLeft = 0;
+            
+        break;
+    }
+});
+
+function jump() {
+
+    player.y -= speedUp;
+    ground = false;
+    fallSpeed = 5;
+    airTime += speedUp;
+}
+function movedown() {
+    player.y += speedDown;
+}
+function moveleft() {
+    player.x -= speedLeft;
+}
+function moveright() {
+    player.x += speedRight;
 }
